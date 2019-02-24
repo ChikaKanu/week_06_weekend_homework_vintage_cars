5f7199e53e38ab7b8d03db243bfcf1ff9806d229
Ifeyinwa Kanu <chika.kanu@procompletions.com>
2019-02-24 16:59:39 +0000

    mvp complete no css

---
+++ js/app.js
@@ -0,0 +1,49 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript loaded');
+  const getNewItemForm = document.querySelector('#get-new-item-form');
+  getNewItemForm.addEventListener('submit', handlegetNewItemFormSubmit);
+
+  const deleteAllButton = document.querySelector('#delete-all');
+  deleteAllButton.addEventListener('click', handleDeleteAllClick);
+})
+
+  const handlegetNewItemFormSubmit = function (event){
+  event.preventDefault();
+
+  const inputDisplayNewListItem = createInputDisplayNewListItem(event.target);
+  const displayNewList = document.querySelector('#input-display');
+  displayNewList.appendChild(inputDisplayNewListItem);
+
+  event.target.reset();
+
+}
+
+  const handleDeleteAllClick = function (event) {
+  const displayNewList = document.querySelector('#input-display');
+  displayNewList.innerHTML = '';
+
+  }
+
+  const createInputDisplayNewListItem = function (form) {
+  const inputDisplayNewListItem = document.createElement('h2');
+  inputDisplayNewListItem.classList.add('input-for-test');
+
+  const carName = document.createElement('h3');
+  carName.textContent = form['car-name'].value;
+  inputDisplayNewListItem.appendChild(carName);
+
+  const carMake = document.createElement('h4');
+  carMake.textContent = form['car-make'].value;
+  inputDisplayNewListItem.appendChild(carMake);
+
+  const carModel = document.createElement('h4');
+  carModel.textContent = form['car-model'].value;
+  inputDisplayNewListItem.appendChild(carModel);
+
+  const carYear = document.createElement('h4');
+  carYear.textContent = event.target['car-year'].value;
+  inputDisplayNewListItem.appendChild(carYear);
+
+  return inputDisplayNewListItem;
+}
